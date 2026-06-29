df08fde241933415254ca7e3f0aaad6cabad9b23
misa116 <misaelvelazquez0227@gmail.com>
2026-06-28 22:12:09 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -457,8 +457,11 @@ app.use(
 
 
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true, limit: "15mb" }));
+
+
+
 
 
 import path from "path";
